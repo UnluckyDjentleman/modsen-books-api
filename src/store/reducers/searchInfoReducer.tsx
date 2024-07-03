ffe8644528333bfcebf3ca44819ce756e0fd6327
dalvhikki <v.goroshchenja@gmail.com>
2024-07-04 00:49:42 +0300

    🤮☠️

---
+++ src/store/reducers/searchInfoReducer.tsx
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import SearchInput from "../../constants/types/search";
+
+const initialState: SearchInput={query:"", order:"relevance", category:"", startIndex: 0}
+
+export const searchInfoReducer=createSlice({
+    name: 'searchInfo',
+    initialState,
+    reducers:{
+        setSearchInfo: (state, action: PayloadAction<SearchInput>)=>{
+            state.query=action.payload.query,
+            state.order=action.payload.order,
+            state.category=action.payload.category,
+            state.startIndex=action.payload.startIndex
+        },
+        changeStartIndex:(state, action: PayloadAction<{count: number}>)=>{
+            state.startIndex+=action.payload.count
+        }
+    }
+})
+
+export const {setSearchInfo, changeStartIndex} = searchInfoReducer.actions;
+
+export default searchInfoReducer.reducer
--- src/utils/booksApi.tsx
+++ src/utils/booksApi.tsx
@@ -1,29 +1,20 @@
 import axios from "axios";
-import BookSearchResult from "../constants/bookSearchResult";
-import BookSearch from "../constants/bookSearch";
+import BookSearchResult from "../constants/types/bookSearchResult";
+import SearchInput from "../constants/types/search";
+
 
 export default class BooksAPI{
-    public static async GetBooks(query: string, startedIndex: number, orderedValue: string|undefined):Promise<BookSearchResult>
+    public static async GetBooks(searchInfo: SearchInput):Promise<BookSearchResult>
     {
-        let resp=await axios.get('https://www.googleapis.com/books/v1/volumes',{
+        let resp=await axios.get(process.env.API_URL as string,{
             params:{
-                q: query,
-                orderBy: orderedValue,
-                startIndex: 30*startedIndex,
+                q: searchInfo.category===""?searchInfo.query:searchInfo.query+"+subject:"+searchInfo.category,
+                orderBy: searchInfo.order,
+                startIndex: searchInfo.startIndex,
                 maxResults: 30,
-                key: 'AIzaSyBRKOSka7VWVxHO-yt3GAiG9GdcideKCSQ' 
-            }
-        })
-        console.log("STATUS:"+resp);
-        return resp.status===200?{bookSearchRes: resp.data.items, totalItems: resp.data.totalItems, error:undefined}:{bookSearchRes: [], totalItems: 0, error: resp.data.response.data.error};
-    }
-
-    public static async GetBook(id: string):Promise<BookSearch>{
-        let resp=await axios.get('https://www.googleapis.com/books/v1/volumes/'+id,{
-            params:{
-                key: 'AIzaSyBRKOSka7VWVxHO-yt3GAiG9GdcideKCSQ'
+                key: process.env.API_KEY
             }
         });
-        return resp.data;
+        return resp.data
     }
 }
