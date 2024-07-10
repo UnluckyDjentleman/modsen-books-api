6b75940e2822a9b44b67f88903a45df44c0b4b3d
dalvhikki <v.goroshchenja@gmail.com>
2024-07-10 16:53:50 +0300

    fix: repairments of bookWithInfo, card and filter

--- src/components/bookWithInfo/bookWithInfo.tsx
+++ src/components/bookWithInfo/bookWithInfo.tsx
@@ -1,46 +1,31 @@
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router';
 
 import photo from '../../assets/book_placeholder.png'
 import Book from '../../constants/types/book'
 
+
 export default function BookWithInfo({ volumeInfo }: { volumeInfo: Book }) {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const onReturn = () => {
-        navigate('/')
+        navigate("/");
     }
     return (
         <>
             <div className="row justify-content-center row-cols-sm-2">
                 <div>
                     <div className="h-100 d-flex align-items-center justify-content-center bg-">
-                        <img
-                            src={
-                                volumeInfo.imageLinks !== undefined
-                                    ? volumeInfo.imageLinks.thumbnail
-                                    : photo
-                            }
-                        ></img>
+                        <img src={volumeInfo.imageLinks !== undefined ? volumeInfo.imageLinks.thumbnail : photo}></img>
                     </div>
                 </div>
                 <div>
-                    <p>
-                        {volumeInfo.categories !== undefined
-                            ? volumeInfo.categories[0]
-                            : ''}
-                    </p>
+                    <p>{volumeInfo.categories !== undefined ? volumeInfo.categories[0] : ''}</p>
                     <h2>{volumeInfo.title}</h2>
-                    <p>
-                        {volumeInfo.authors !== undefined
-                            ? volumeInfo.authors.toString()
-                            : ''}
-                    </p>
+                    <p>{volumeInfo.authors !== undefined ? volumeInfo.authors.toString() : ''}</p>
                     <div className="border">
                         <p>{volumeInfo.description}</p>
                     </div>
-                    <button onClick={onReturn} className="btn btn-link">
-                        Back
-                    </button>
+                    <button onClick={onReturn} className="btn btn-link">Back</button>
                 </div>
             </div>
         </>
--- src/components/header/filter/filter.tsx
+++ src/components/header/filter/filter.tsx
@@ -2,26 +2,18 @@ import { useCallback } from 'react'
 
 import FilterElement from '../../../constants/types/filterElement'
 
-export default function Filter({
-    info,
-    onSelection,
-}: {
-    info: FilterElement
-    onSelection: (value: string) => void
-}) {
-    const onSelected = useCallback(
-        (e: React.ChangeEvent<HTMLSelectElement>) => {
-            if (onSelection) onSelection(e.target.value)
-        },
-        [onSelection],
-    )
+export default function Filter({ info, onSelection }: { info: FilterElement, onSelection: (value: string) => void }) {
+    const onSelected = useCallback((e: React.ChangeEvent<HTMLSelectElement>) => {
+        if (onSelection) onSelection(e.target.value);
+    }, [onSelection])
 
     return (
         <select className="selectpicker" onChange={onSelected}>
-            {info.items &&
-                info.items.map(el => (
-                    <option value={el !== 'all' ? el : ''}>{el}</option>
-                ))}
+            {
+                info.items && info.items.map((el) => (
+                    <option value={el !== 'all' ? el : ""}>{el}</option>
+                ))
+            }
         </select>
     )
 }
